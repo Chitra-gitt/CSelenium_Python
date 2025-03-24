8edd45187a9527badaa4a2314154b1442e4e50bf
Bangaru <Bangaru@DESKTOP-OEUBE0Q>
2025-03-24 09:16:15 +0530

    First commit

---
+++ chitrapython_tutorials/basics/first_program.py
@@ -0,0 +1,8 @@
+'''
+Created on Mar 18, 2025
+
+@author: Chitra
+'''
+print("hello world")
+
+
---
+++ chitrapython_tutorials/basics/variables.py
@@ -0,0 +1,18 @@
+'''
+Created on Mar 19, 2025
+
+@author: Chitra
+'''
+age=32
+num1=676
+num2=1205
+
+print(age)
+print(num1)
+print(num2)
+
+num3, num4 = 102, 789
+print(num4)
+
+num5=num6= 56
+print(num5,num6)
