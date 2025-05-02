fd947e4b81e08320cba48adbd6131c41f24cbf6d
siddharth <siddharth@siddharths-mbp>
2025-05-02 10:05:02 +0530

    functions

---
+++ chitrapython_tutorials/functions/my_functions.py
@@ -0,0 +1,96 @@
+'''
+Created on 2 May 2025
+
+@author: Chitra
+
+
+Function: Is a block of code which can be reused
+OR
+Function is a block of reusable code
+
+Ex: print(), type(), input(), id()...
+
+Types of functions:
+1. Predefined functions- came as part of python package
+2. User-defined functions- are defined by users
+
+Why we need functions?
+1. Reduce repetition of code/ reduce complexity, minimize the no. of lines of code. This helps in increased code readability
+2. Easy maintanence
+
+Syntax:
+
+def function_name(parameters):
+    <code>
+
+def -> keyword to define function (mandatory)
+function_name -> lower case words separated by _ (mandatory)
+(): -> mandatory
+parametrs -> variables to accept/ consume the values passed by the user (not mandatory)
+
+Types:
+1. Function without parameters
+2. Function with parameters
+'''
+
+a=2
+b=5
+c=a+b 
+print(f"Sum of {a} and {b}:", c)
+
+a=6
+b=7
+c=a+b 
+print(f"Sum of {a} and {b}:", c)
+
+# Function without parameters
+def welcome():
+    print("Hello User! Welcome to iQuest!")
+    print("I hope you're doing great!")
+
+welcome()
+# welcome()
+# welcome()
+# welcome()
+
+
+#Function with parameters
+def addition(a, b):
+    c=a+b 
+    # print(f"Addition of {a} and {b}:", c)
+    return c
+    
+d=addition(10, 20)
+print(d)
+
+print(addition(8, 10))
+
+
+def multiplication(num,multiplier):
+    result_mul=num*multiplier
+    # print(result_mul)
+    return result_mul
+    
+multiplication(addition(10, 20), 5) # passing a function as parameter to another function
+
+def division_integer_division(dividend, divisor):
+    div_quotient = dividend/divisor
+    int_div_quotient = dividend//divisor
+    return div_quotient, int_div_quotient
+
+div_output1, div_output2 =division_integer_division(7, 2)
+print(div_output1)
+print(div_output2)
+
+
+
+f, g=5,6
+print(f)
+print(g)
+
+def add_mul(a, b):
+    add_op=addition(a, b) # calling one function inside another function
+    mul_op=multiplication(a, b)
+    return add_op, mul_op
+    
+print(add_mul(5, 3))
