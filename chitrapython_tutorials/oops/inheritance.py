d1ffeb539c1b217375deadffda1de4021faa1c13
siddharth <siddharth@siddharths-mbp>
2025-05-11 12:57:19 +0530

    inheritance

--- chitrapython_tutorials/oops/inheritance.py
+++ chitrapython_tutorials/oops/inheritance.py
@@ -26,25 +26,28 @@ Method resolution order(MRO)- In an inheritance when we call a method using  an 
 MRO will help python interpreter to decide in which order it has to traverse the class.
 
 "object class" is the super-class of all the classes in the python.
+object class object() is parent class of all the classes.
 
-
-
+super class keyword- super() is used to call super class/ parent class constructors 
+or methods to child class when there is a constructor or method present in child class with same 
+name as super class.
 '''
 
 
-
-
-class Grandfather:
-    def __init__(self):
-        print("This is grandfather class constructor")
+class GrandFather:
+    def __init__(self, name):
+        print("`object is created with name:", name)
         
     def gf_method(self):
         print("This is Grandfather method")
         
-class Mother(Grandfather):
-    def __init__(self):
-        print("This is mother class constructor")
-        
+class Mother(GrandFather):
+    def __init__(self, name, age):
+        #print(f"This is mother name: {name}, age: {age}")
+        super().__init__(name)
+        print(f"Age of {name} is {age}")
+
+
     def m_method(self):
         print("This is Mother class m_ method")
         
@@ -60,33 +63,33 @@ class Father():
             
         
 class Child(Mother,Father):
-    def __init__(self):
-        print("This is child class constructor")
+#     def __init__(self):
+#         print("This is child class constructor")
         
     def c_method(self):
         print("This is Child class c_method")
 
 
 print("==========Grandfather class==========")        
-ajja=Grandfather()
-ajja.gf_method()
+ajja=GrandFather("tatha")
+# ajja.gf_method()
 
 print("==========Mother class==========")        
-amma=Mother()
-amma.m_method()
-amma.gf_method()
+amma=Mother("amma", 55)
+# amma.m_method()
+# amma.gf_method()
 
 print("==========Child class==========")        
-baby=Child()
-baby.c_method()
-baby.gf_method()
-baby.m_method()
-baby.f_method()
-baby.car()
+baby=Child("baby", 32)
+# baby.c_method()
+# baby.gf_method()
+# baby.m_method()
+# baby.f_method()
+# baby.car()
 
 print("=========This is Method resolution class(MRO)=========")
 print(Child.mro())
 
-
-
+print("=========Directory method(dir)==========")
+print(dir(baby))
 
